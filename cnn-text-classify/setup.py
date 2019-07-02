02e9c73d42b5b60caa95063b541931e1d01b91f7
Si Chen <t-chsi@microsoft.com>
2019-07-02 18:46:04 +0800

    first version of custom cnn text classify model

---
+++ cnn-text-classify/setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+    name="cnn-text-classify",
+    version="0.0.1",
+    description="CNN Text Classification",
+    packages=["script"],
+    author="Si Chen",
+    license="MIT",
+    include_package_data=True,
+)
