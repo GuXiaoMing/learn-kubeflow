5caf81a32b868bcdd8ded261fc03cfd3d3898a37
Si Chen <t-chsi@microsoft.com>
2019-07-05 13:28:47 +0800

    upgrade setup.py to 2

--- cnn-text-classify/setup.py
+++ cnn-text-classify/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 
 setup(
     name="cnn-text-classify",
-    version="0.0.1",
+    version="0.0.2",
     description="CNN Text Classification",
     packages=["csmodel"],
     author="Si Chen",
